ad6c0db8c30cb4940838f83204cb167154c790ae
JAIME TIAN WONG <JAWONG8@sfusd.edu>
2017-10-17 11:34:29 -0700

    hi

--- Farm.java
+++ Farm.java
@@ -1,4 +1,12 @@
-class Farm 
-{     
-   //your code here
-}
+class Farm  {     
+   private Animal[] aBunchOfAnimals = new Animal[3];    
+   public Farm()     {       
+      aBunchOfAnimals[0] = new Cow("Cow","Moo");           
+      aBunchOfAnimals[1] = new Chick("Chick","Cluck");       
+      aBunchOfAnimals[2] = new Pig("Pig","Oink");    }         
+   public void animalSounds()    {       
+     for (int nI=0; nI < aBunchOfAnimals.length; nI++)       {          
+       System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());       
+      }    
+    } 
+} 
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,11 @@
-
-void setup()
-{
-	//your variable declarations here
+void setup() {
+  
+  Cow c = new Cow("Cow", "Moo");
+  System.out.println(c.getType() + " goes " + c.getSound());
+  
+  Chick h = new Chick("Chick", "Cluck");
+  System.out.println(h.getType() + " goes " + h.getSound());
+  
+  Pig p = new Pig("Pig", "Oink");
+  System.out.println(p.getType() + " goes " + p.getSound());
 }
-
